b3ae235a681da31b5a4e0ab2766d689a085c0d36
Manojkumar962952 <126284997+Manojkumar962952@users.noreply.github.com>
2024-09-24 12:27:25 +0530

    Update dbconnection.php

--- bpms/admin/includes/dbconnection.php
+++ bpms/admin/includes/dbconnection.php
@@ -1,7 +1,6 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "bpmsdb");
-if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
+$con = mysqli_connect("localhost", "bpmsuser", "strong_password", "bpmsdb");
+if (mysqli_connect_errno()) {
+    echo "Connection Fail: " . mysqli_connect_error();
 }
-
-  ?>
+?>
